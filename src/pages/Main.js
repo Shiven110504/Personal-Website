7b3168d6c21b3596d335824bb1af9beacc45808c
Shiven Patel <shiven110504@gmail.com>
2024-04-24 00:44:23 -0400

    Added Footer Section

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import Skills from "../components/Sections/Skills";
 import ContactForm from "../components/Sections/ContactForm";
 import Hero from "../components/Sections/Hero";
 import Projects from "../components/Sections/Projects";
+import Footer from "../components/Sections/Footer";
 
 const Main = () => {
   useEffect(() => {
@@ -45,22 +46,7 @@ const Main = () => {
       <Skills />
       <Projects />
       <ContactForm />
-      <footer className="self-stretch bg-primary-black overflow-hidden flex flex-row items-start justify-start py-6 pr-[97px] pl-20 box-border max-w-full text-right text-base text-primary-white font-heading-h6-semibold mq825:pl-10 mq825:pr-12 mq825:box-border mq450:pr-5 mq450:box-border">
-        <div className="flex-1 overflow-hidden flex flex-row items-center justify-between py-0 px-8 box-border gap-[20px] max-w-full mq825:flex-wrap">
-          <img
-            className="relative w-[122px] h-6"
-            loading="eager"
-            alt=""
-            src="/logo.svg"
-          />
-          <div className="flex flex-col items-end justify-start gap-[12px]">
-            <div className="relative tracking-[-0.02em] leading-[20px] font-semibold">
-              @2024 Shiven
-            </div>
-            <div className="relative tracking-[-0.02em] leading-[20px] font-semibold">{`Made with Love and Coffee `}</div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };

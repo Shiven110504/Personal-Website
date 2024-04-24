34f5f497caedfd570a6b81abd5262917b93fc3b9
Shiven Patel <shiven110504@gmail.com>
2024-04-24 00:16:38 -0400

    Changed Skills

--- src/components/Sections/Skills.js
+++ src/components/Sections/Skills.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Skill from "../SkillComponent";
 
 const Skills = () => {
   useEffect(() => {
@@ -30,6 +31,20 @@ const Skills = () => {
       }
     };
   }, []);
+
+  const skills = [
+    { src: "/python.svg", label: "Python" },
+    { src: "/iconjavscript.svg", label: "JavaScript" },
+    { src: "/html5.svg", label: "HTML" },
+    { src: "/css3.svg", label: "CSS" },
+    { src: "/react.svg", label: "React.js" },
+    { src: "/tailwindcss.svg", label: "Tailwind" },
+    { src: "/nodejs.svg", label: "Node.js" },
+    { src: "/nextjs.svg", label: "Next.js" },
+    { src: "/java.svg", label: "Java" },
+    { src: "/icongit.svg", label: "Git/Github" }
+  ];
+
   return (
     <section
       className="self-stretch overflow-hidden flex flex-col items-center justify-center py-[60px] px-20 text-left text-29xl text-primary-black font-heading-h6-semibold mq825:py-[39px] mq825:px-10 mq825:box-border"
@@ -48,146 +63,9 @@ const Skills = () => {
           </h1>
         </div>
         <div className="self-stretch flex flex-row flex-wrap items-center justify-center py-5 px-0 box-border gap-[71px] min-h-[492px] text-center text-xl mq825:gap-[35px] mq450:gap-[18px]">
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14 overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/python.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              Python
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14"
-              loading="eager"
-              alt=""
-              src="/iconjavscript.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              javascript
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14 overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/html5.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              HTML
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14"
-              loading="eager"
-              alt=""
-              src="/css3.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              CSS
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14 overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/react.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              React.js
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14"
-              loading="eager"
-              alt=""
-              src="/tailwindcss.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              Tailwind
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14 overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/nodejs.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              Node.js
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14"
-              loading="eager"
-              alt=""
-              src="/nextjs.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              Next.js
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14 overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/java.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              Java
-            </b>
-          </div>
-          <div
-            className="rounded bg-primary-white box-border w-[186px] h-[186px] flex flex-col items-center justify-center py-6 px-[22px] gap-[32px] [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] border-[2px] border-solid border-primary-black"
-            data-animate-on-scroll
-          >
-            <img
-              className="relative w-14 h-14 overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/icongit.svg"
-            />
-            <b className="self-stretch relative tracking-[-0.02em] leading-[24px] capitalize mq450:text-base mq450:leading-[19px]">
-              Git/Github
-            </b>
-          </div>
+          {skills.map(skill => (
+          <Skill key={skill.label} src={skill.src} label={skill.label} animateOnScroll={true} />
+        ))}
         </div>
       </div>
     </section>
